b40556073e4732f44c3474e7e7055ec2f984af4f
Manab Chetia <manab.chetia@outlook.com>
2015-07-13 14:46:44 +1000

    Sydney location shows up

---
+++ GMapsDemo/MapTasks.swift
@@ -0,0 +1,13 @@
+//
+//  MapTasks.swift
+//  GMapsDemo
+//
+//  Created by Manab Chetia on 13/07/2015.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import Cocoa
+
+class MapTasks: NSObject {
+
+}
